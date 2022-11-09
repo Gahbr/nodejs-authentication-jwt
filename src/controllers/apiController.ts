d455cdc3ea436e074836682e00c10ebe7364116d
Gabriel Ribeiro <80289718+Gahbr@users.noreply.github.com>
2022-11-08 21:48:44 -0300

    feat: fazendo o login e validando senha encriptada

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -47,29 +47,31 @@ export const login = async (req: Request, res: Response) => {
     if(req.body.email && req.body.password) {
         let email: string = req.body.email;
         let password: string = req.body.password;
-  /* const validPassword = await bcrypt.compare(password, novaSenha);
-            if (validPassword) {
-              console.log("Senha confere");
-            } else {
-              console.log("senha invalida");
-            } */
+        
         let user = await User.findOne({ 
-            where: { email, password }
+            where: { email }
         });
 
         if(user) {
-            const token = JWT.sign(
-                {id:user.id, email:user.email},
-                process.env.JWT_SECRET_KEY as string,
-                {expiresIn:'2h'}
-            );
+
+            const validPassword = await bcrypt.compareSync(password, user.password);
+            if (validPassword) {
+                console.log("senha valida",validPassword);
+                const token = JWT.sign(
+                    {id:user.id, email:user.email},
+                    process.env.JWT_SECRET_KEY as string,
+                    {expiresIn:'2h'}
+                );
+                res.json({ status: true, token });
+            } else {
+              res.json("senha invalida");
+            }
             
-            res.json({ status: true, token });
             return;
         }
-    }
+    }else {res.json({ status: false });}
 
-    res.json({ status: false });
+    
 }
 
 export const list = async (req: Request, res: Response) => {
